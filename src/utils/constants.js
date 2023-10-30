5a9f18c2f3f691b40072a06148beef48299eaff3
RibhuGuha <ribhu@replicacia.com>
2023-10-30 20:13:38 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "http://3.92.222.200:8086/ExclusiveAccess"; //Temporarily using field; Enter Backend URL here e.g. http://3.133.64.67:8164/generated_app
+export const PATH_PAYMENT = "/Payments";
+export const PATH_SUBSCRIPTION = "/Subscriptions";
